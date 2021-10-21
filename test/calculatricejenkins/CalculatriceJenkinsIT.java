25b0311d9d3526b260821e7c6331b7c9499c6a6d
ald <alexis.ledain@itesoft.com>
2021-10-21 11:48:36 +0200

    hamrest added

--- test/calculatricejenkins/CalculatriceJenkinsIT.java
+++ test/calculatricejenkins/CalculatriceJenkinsIT.java
@@ -45,7 +45,6 @@ public class CalculatriceJenkinsIT {
         String[] args = null;
         CalculatriceJenkins.main(args);
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
     
 }
